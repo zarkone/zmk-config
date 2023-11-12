8785513be8b1d52fd08e426926a7c222cbbb62b9
Anatolii Smolianinov <zarkonesmall@gmail.com>
2023-11-13 00:23:01 +0100

    add flash script

---
+++ flash.sh
@@ -0,0 +1,4 @@
+#/usr/bin/env sh
+rm -rf ~/Downloads/firmware/ 
+unzip ~/Downloads/firmware.zip -d ~/Downloads/firmware/
+cp ~/Downloads/firmware/corneish_zen_v2_left-zmk.uf2 /run/media/zarkone/CORNEISHZEN/
